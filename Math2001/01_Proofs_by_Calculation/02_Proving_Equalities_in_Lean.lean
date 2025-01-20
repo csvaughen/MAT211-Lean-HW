a753aa4871dab646bdd598d0b8d1ec1bc3045d71
csvaughen <csvaughen@gmail.com>
2025-01-20 15:26:10 -0500

    hopefully final version of hw1

--- Math2001/01_Proofs_by_Calculation/02_Proving_Equalities_in_Lean.lean
+++ Math2001/01_Proofs_by_Calculation/02_Proving_Equalities_in_Lean.lean
@@ -1,4 +1,6 @@
 /- Copyright (c) Heather Macbeth, 2022.  All rights reserved. -/
+/- This is an abbreviated version of Dr. Macbeth's work adapted by Riccardo Brasca -/
+
 import Mathlib.Data.Real.Basic
 import Library.Basic
 
@@ -23,18 +25,21 @@ example {a b : ℚ} (h1 : a - b = 4) (h2 : a * b = 1) :
   done
 
 -- Example 1.2.2.
--- Exercise: replace sorry with appropriate tactic
+-- "sorry" is a placeholder, it tells Lean we aren't done
+-- Exercise: replace "sorry" with appropriate tactic
+
 example {r s : ℝ} (h1 : s = 3) (h2 : r + 2 * s = -1) :
 r = -7 := by
   calc
-    r = r + 2 * s - 2 * s := by ring
-    _ = -1 - 2 * s := by rw[h2]
-    _ = -1 - 2 * 3 := by rw[h1]
-    _ = -7 := by ring
+    r = r + 2 * s - 2 * s := by sorry
+    _ = -1 - 2 * s := by sorry
+    _ = -1 - 2 * 3 := by sorry
+    _ = -7 := by sorry
   done
 
 -- Example 1.2.4.
--- Exercise : replace "sorry" with the complete proof
+-- Exercise : replace "sorry" below with the complete proof
+
 example {a b c d e f : ℤ} (h1 : a * d = b * c) (h2 : c * f = d * e) :
     d * (a * f - b * e) = 0 := by
   sorry
